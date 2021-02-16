dc13bf58460e5dce8ab8ec94afd3f35783881a72
yun <yun160308@gmail.com>
2021-02-16 16:33:25 +0800

    增加基本排序算法相关内容

---
+++ src/main/java/basicalgorithm/sort/InsertSort.java
@@ -0,0 +1,34 @@
+package basicalgorithm.sort;
+
+import java.util.Arrays;
+
+/**
+ * 插入排序
+ */
+public class InsertSort {
+
+    public static void main(String[] args) {
+        int[] a = new int[]{
+                3, 5, 6, 3, 8, 5, 3, 7, 2, 0, 5, 9, 3, 1, 0
+        };
+        int[] b = insertSort(a);
+        System.out.println(Arrays.toString(b));
+    }
+
+    /**
+     * 插入排序
+     */
+    public static int[] insertSort(int[] a) {
+        for (int i = 1; i < a.length; i++) {
+            int j = i - 1;
+            int key = a[i];
+            while (j >= 0 && a[j] > key) {
+                a[j + 1] = a[j];
+                j--;
+            }
+            a[j + 1] = key;
+        }
+        return a;
+    }
+
+}
