16f9e5725afc491c34c07aae1c85f4b414e6e613
yun <yun160308@gmail.com>
2022-07-05 23:27:18 +0800

    新增https://leetcode.cn/problems/linked-list-cycle-ii/问题的求解

---
+++ src/main/java/basicalgorithm/linkedList/leetcode142/Solution.java
@@ -0,0 +1,64 @@
+package basicalgorithm.linkedList.leetcode142;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.cn/problems/linked-list-cycle-ii/
+ */
+public class Solution {
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        List<ListNode> nodeList = new ArrayList<>();
+        ListNode cur = head;
+        while (cur != null) {
+            if (nodeList.contains(cur)) {
+                return cur;
+            } else {
+                nodeList.add(cur);
+            }
+            cur = cur.next;
+        }
+        return null;
+    }
+
+    /**
+     * 快慢指针
+     * 详情见leetcode题解
+     */
+    public ListNode detectCycle2(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                ListNode entry = head;
+                while (slow != entry) {
+                    slow = slow.next;
+                    entry = entry.next;
+                }
+                return entry;
+            }
+        }
+        return null;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
