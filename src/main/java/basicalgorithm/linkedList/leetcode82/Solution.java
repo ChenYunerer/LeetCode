2dd089e352a1aa4077e150ad6a5c2f35efc3aaa7
yunyu.cy <yunyu.cy@alibaba-inc.com>
2022-07-05 21:02:49 +0800

    新增https://leetcode.cn/problems/remove-duplicates-from-sorted-list-ii/问题的求解

--- src/main/java/basicalgorithm/linkedList/leetcode82/Solution.java
+++ src/main/java/basicalgorithm/linkedList/leetcode82/Solution.java
@@ -9,10 +9,10 @@ public class Solution {
         Solution solution = new Solution();
         ListNode listNode1 = solution.genListNode(new int[]{1, 2, 3, 3, 4, 4, 5});
         ListNode listNode2 = solution.genListNode(new int[]{1, 1, 1, 2, 3, 4, 5});
-        ListNode listNode3 = solution.genListNode(new int[]{1, 1, 1});
+        ListNode listNode3 = solution.genListNode(new int[]{1, 1, 1, 3, 3});
         ListNode listNode4 = solution.genListNode(new int[]{1, 2, 2});
-        solution.printLinkNode(listNode3);
-        ListNode result = solution.deleteDuplicates(listNode3);
+        solution.printLinkNode(listNode4);
+        ListNode result = solution.deleteDuplicates(listNode4);
         solution.printLinkNode(result);
     }
 
@@ -41,45 +41,50 @@ public class Solution {
     }
 
     public ListNode deleteDuplicates(ListNode head) {
-        if (head == null) {
-            return null;
-        }
-        if (head.next == null) {
+        if (head == null || head.next == null) {
             return head;
         }
-        ListNode resultHead = null;
-        ListNode pre = null;
-        ListNode cur = head;
-        Integer duplicateValue = null;
-        while (cur.next != null) {
-            if (duplicateValue != null && cur.val == duplicateValue) {
+        // 因为最终链表头可能变化 所以搞一个虚拟头
+        ListNode vHead = new ListNode();
+        vHead.next = head;
+
+        ListNode pre = vHead;
+        ListNode cur = vHead.next;
+        while (cur != null) {
+            // 假定当前就是重复的第一个
+            int duplicateValue = cur.val;
+            // 有点类似于滑动窗口，找到最后一个重复的，cur移动到最后一个重复的位置
+            while (cur.next != null && cur.next.val == duplicateValue) {
                 cur = cur.next;
-                if (pre != null) {
-                    pre.next = cur;
-                } else {
-                    resultHead = cur;
-                }
-            } else {
-                if (cur.val == cur.next.val) {
-                    duplicateValue = cur.val;
-                    cur = cur.next;
-                    if (pre != null) {
-                        pre.next = cur;
-                    }
-                } else {
-                    if (resultHead == null) {
-                        resultHead = cur;
-                    }
-                    duplicateValue = null;
-                    pre = cur;
-                    cur = cur.next;
-                }
             }
+            // 判断pre和cur是否还是连续
+            if (pre.next == cur) {
+                // 如果pre和cur还是连续，说明上面并没有重复
+                // 只要把pre和cur往前移动一步然后继续大循环就可以了
+                pre = cur;
+                cur = cur.next;
+            } else {
+                // 如果pre和cur不连续了，说明中间出现了连续值
+                // cur当前处在最后一个连续值的位置，并且这个节点也是需要删除的
+                // 所以cur前进一步，并且pre.next指向cur(等于删除中间所有重复项)
+                cur = cur.next;
+                pre.next = cur;
+            }
+
         }
-        if (duplicateValue != null && cur.val == duplicateValue && pre != null) {
-            pre.next = null;
+        return vHead.next;
+    }
+
+    public ListNode removeSameHead(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
         }
-        return resultHead;
+        ListNode cur = head;
+        int duplicateValue = head.val;
+        while (cur.next != null && cur.next.val == duplicateValue) {
+            cur = cur.next;
+        }
+        return cur.next;
     }
 
 
