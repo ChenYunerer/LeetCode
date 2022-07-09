fae82ae51d66779aadb730b3eea8e32121ba6592
yun <yun160308@gmail.com>
2022-07-09 21:34:12 +0800

    新增leetcode[617. 合并二叉树](https://leetcode.cn/problems/merge-two-binary-trees/)问题的求解

--- src/main/java/basicalgorithm/recursion/leetcode617/Solution.java
+++ src/main/java/basicalgorithm/recursion/leetcode617/Solution.java
@@ -13,10 +13,11 @@ public class Solution {
         if (root1 == null || root2 == null) {
             return root1 != null ? root1 : root2;
         }
-        root1.val = root1.val + root2.val;
-        root1.left = mergeTrees(root1.left, root2.left);
-        root1.right = mergeTrees(root1.right, root2.right);
-        return root1;
+        TreeNode root = new TreeNode();
+        root.val = root1.val + root2.val;
+        root.left = mergeTrees(root1.left, root2.left);
+        root.right = mergeTrees(root1.right, root2.right);
+        return root;
     }
 
     public class TreeNode {
