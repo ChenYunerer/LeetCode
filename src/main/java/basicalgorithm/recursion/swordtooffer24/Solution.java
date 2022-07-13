432b56ab87125478ae8df50924977d749f0f9a33
yunyu.cy <yunyu.cy@alibaba-inc.com>
2022-07-13 19:24:35 +0800

    新增leetcode[剑指 Offer 24. 反转链表](https://leetcode.cn/problems/fan-zhuan-lian-biao-lcof/)问题的递归求解

---
+++ src/main/java/basicalgorithm/recursion/swordtooffer24/Solution.java
@@ -0,0 +1,30 @@
+package basicalgorithm.recursion.swordtooffer24;
+
+/**
+ * 剑指 Offer 24. 反转链表
+ * https://leetcode.cn/problems/fan-zhuan-lian-biao-lcof/
+ *
+ * @author yunyu.cy
+ */
+public class Solution {
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
