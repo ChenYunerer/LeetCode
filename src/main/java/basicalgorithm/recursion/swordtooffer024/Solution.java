072defe973fc33ef14d6605c1ac215864513c547
yun <yun160308@gmail.com>
2022-07-18 21:52:53 +0800

    新增leetcode [剑指 Offer II 024. 反转链表](https://leetcode.cn/problems/UHnkqh/) 问题的递归求解

---
+++ src/main/java/basicalgorithm/recursion/swordtooffer024/Solution.java
@@ -0,0 +1,36 @@
+package basicalgorithm.recursion.swordtooffer024;
+
+/**
+ * 剑指 Offer II 024. 反转链表
+ * https://leetcode.cn/problems/UHnkqh/
+ */
+public class Solution {
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
