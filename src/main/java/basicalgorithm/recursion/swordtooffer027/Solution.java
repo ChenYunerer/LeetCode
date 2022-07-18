1916b6460651d5a9f19d11c7710218625cd19cb9
yun <yun160308@gmail.com>
2022-07-18 22:29:00 +0800

    新增leetcode [剑指 Offer II 027. 回文链表](https://leetcode.cn/problems/aMhZSa/) 问题的递归求解

---
+++ src/main/java/basicalgorithm/recursion/swordtooffer027/Solution.java
@@ -0,0 +1,48 @@
+package basicalgorithm.recursion.swordtooffer027;
+
+/**
+ * 剑指 Offer II 027. 回文链表
+ * https://leetcode.cn/problems/aMhZSa/
+ */
+public class Solution {
+
+    private ListNode cur;
+
+    private boolean doIsPalindrome(ListNode head) {
+        if (head == null) {
+            return true;
+        }
+        boolean isPalindrome = doIsPalindrome(head.next);
+        if (!isPalindrome) {
+            return false;
+        }
+        if (head.val != cur.val) {
+            return false;
+        } else {
+            cur = cur.next;
+            return true;
+        }
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        cur = head;
+        return doIsPalindrome(head);
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
