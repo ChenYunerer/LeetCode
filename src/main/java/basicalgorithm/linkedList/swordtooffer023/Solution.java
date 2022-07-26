7a5a1b6ac735802be2ea9ebf168d0986f3f18fdf
yun <yun160308@gmail.com>
2022-07-26 23:20:58 +0800

    新增leetcode [剑指 Offer II 023. 两个链表的第一个重合节点](https://leetcode.cn/problems/3u1WK4/) 问题的求解

---
+++ src/main/java/basicalgorithm/linkedList/swordtooffer023/Solution.java
@@ -0,0 +1,33 @@
+package basicalgorithm.linkedList.swordtooffer023;
+
+/**
+ * 剑指 Offer II 023. 两个链表的第一个重合节点
+ * https://leetcode.cn/problems/3u1WK4/
+ */
+public class Solution {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode aCur = headA;
+        while (aCur != null) {
+            ListNode bCur = headB;
+            while (bCur != null) {
+                if (aCur == bCur) {
+                    return aCur;
+                }
+                bCur = bCur.next;
+            }
+            aCur = aCur.next;
+        }
+        return null;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
