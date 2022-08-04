8ff627df60b738c3709c4d6d67001904a2d331db
yun <yun160308@gmail.com>
2022-08-05 00:06:44 +0800

    新增leetcode [94. 二叉树的中序遍历](https://leetcode.cn/problems/binary-tree-inorder-traversal/) 问题的求解

---
+++ src/main/java/basicalgorithm/tree/leetcode94/Solution2.java
@@ -0,0 +1,51 @@
+package basicalgorithm.tree.leetcode94;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 94. 二叉树的中序遍历
+ * https://leetcode.cn/problems/binary-tree-inorder-traversal/
+ */
+public class Solution2 {
+
+    /**
+     * 迭代
+     * todo review 搞不懂
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Deque<TreeNode> stk = new LinkedList<>();
+        while (root != null || !stk.isEmpty()) {
+            while (root != null) {
+                stk.push(root);
+                root = root.left;
+            }
+            root = stk.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+        return res;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
