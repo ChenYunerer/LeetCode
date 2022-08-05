5a97e4621809d69fb9290d125de13f0f4477edca
yun <yun160308@gmail.com>
2022-08-05 22:56:13 +0800

    新增leetcode [102. 二叉树的层序遍历](https://leetcode.cn/problems/binary-tree-level-order-traversal/) 问题的求解

---
+++ src/main/java/basicalgorithm/tree/leetcode102/Solution.java
@@ -0,0 +1,63 @@
+package basicalgorithm.tree.leetcode102;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 102. 二叉树的层序遍历
+ * https://leetcode.cn/problems/binary-tree-level-order-traversal/
+ */
+public class Solution {
+
+    /**
+     * DFS
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            Deque<TreeNode> subQueue = new LinkedList<>();
+            while (!queue.isEmpty()) {
+                subQueue.offer(queue.poll());
+            }
+            List<Integer> subList = new ArrayList<>();
+            while (!subQueue.isEmpty()) {
+                TreeNode node = subQueue.poll();
+                subList.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(subList);
+        }
+        return result;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
