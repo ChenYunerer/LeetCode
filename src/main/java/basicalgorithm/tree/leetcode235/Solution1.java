a84b567697faf6ef4c9d9c30ca3ad8daae3b3918
yun <yun160308@gmail.com>
2022-08-11 00:39:02 +0800

    新增leetcode [235. 二叉搜索树的最近公共祖先](https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-search-tree/) 问题的求解

---
+++ src/main/java/basicalgorithm/tree/leetcode235/Solution1.java
@@ -0,0 +1,21 @@
+package basicalgorithm.tree.leetcode235;
+
+import basicalgorithm.tree.base.TreeNode;
+
+/**
+ * 235. 二叉搜索树的最近公共祖先
+ * https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-search-tree/
+ */
+public class Solution1 {
+
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
+}
