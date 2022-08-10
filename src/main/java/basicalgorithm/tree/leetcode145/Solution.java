55a79eba1b7f053afae4f2ec073342842d544567
yun <yun160308@gmail.com>
2022-08-10 23:34:02 +0800

    新增leetcode [94. 二叉树的中序遍历](https://leetcode.cn/problems/binary-tree-inorder-traversal/) 问题的DFS求解

--- src/main/java/basicalgorithm/tree/leetcode145/Solution.java
+++ src/main/java/basicalgorithm/tree/leetcode145/Solution.java
@@ -50,4 +50,29 @@ public class Solution {
         }
         return res;
     }
+
+    public List<Integer> postorderTraversal1(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode prev = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (root.right != null && root.right != prev) {
+                stack.push(root);
+                root = root.right;
+            } else {
+                res.add(root.val);
+                prev = root;
+                root = null;
+            }
+        }
+        return res;
+    }
 }
