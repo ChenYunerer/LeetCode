6cefaaa4cda10bdd33da2903dee058bdc61bec46
yun <yun160308@gmail.com>
2022-08-18 00:40:24 +0800

    新增leetcode [1372. 二叉树中的最长交错路径](https://leetcode.cn/problems/longest-zigzag-path-in-a-binary-tree/) 问题的DFS求解

---
+++ src/main/java/basicalgorithm/tree/leetcode1372/Solution1.java
@@ -0,0 +1,31 @@
+package basicalgorithm.tree.leetcode1372;
+
+import basicalgorithm.tree.base.TreeNode;
+import basicalgorithm.tree.base.TreeUtil;
+
+/**
+ * 1372. 二叉树中的最长交错路径
+ * https://leetcode.cn/problems/longest-zigzag-path-in-a-binary-tree/
+ */
+public class Solution1 {
+
+
+    public static void main(String[] args) {
+        TreeNode root = TreeUtil.genTreeNode(new Integer[]{1, null, 1, 1, 1, null, null, 1, 1, null, 1, null, null, null, 1, null, 1});
+        Solution1 solution = new Solution1();
+        int result = solution.longestZigZag(root);
+        System.out.println(result);
+    }
+
+    public int longestZigZag(TreeNode root) {
+        return Math.max(dfs(root, -1, -1), dfs(root, 1, -1));
+    }
+
+    private int dfs(TreeNode root, int direction, int length) {
+        if (root == null) {
+            return length;
+        }
+        length = length + 1;
+        return Math.max(dfs(direction == 1 ? root.right : root.left, -direction, length), dfs(direction == 1 ? root.right : root.left, direction, -1));
+    }
+}
