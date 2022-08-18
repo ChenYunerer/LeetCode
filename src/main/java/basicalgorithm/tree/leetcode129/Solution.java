0b12d7f8bf369620cb5e1838f075b2886f461a66
yun <yun160308@gmail.com>
2022-08-19 00:16:21 +0800

    新增leetcode [129. 求根节点到叶节点数字之和](https://leetcode.cn/problems/sum-root-to-leaf-numbers/) 问题的DFS求解

---
+++ src/main/java/basicalgorithm/tree/leetcode129/Solution.java
@@ -0,0 +1,29 @@
+package basicalgorithm.tree.leetcode129;
+
+import basicalgorithm.tree.base.TreeNode;
+
+/**
+ * 129. 求根节点到叶节点数字之和
+ * https://leetcode.cn/problems/sum-root-to-leaf-numbers/
+ */
+public class Solution {
+
+    int ans = 0;
+
+    public int sumNumbers(TreeNode root) {
+        dfs(root, 0);
+        return ans;
+    }
+
+    private void dfs(TreeNode root, int pathVal) {
+        if (root == null) {
+            return;
+        }
+        pathVal = pathVal * 10 + root.val;
+        if (root.left == null && root.right == null) {
+            ans = ans + pathVal;
+        }
+        dfs(root.left, pathVal);
+        dfs(root.right, pathVal);
+    }
+}
