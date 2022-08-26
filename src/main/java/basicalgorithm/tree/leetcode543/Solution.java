956785c484fcf1bf222af97f7f87aa8bb5cc04c3
yun <yun160308@gmail.com>
2022-08-26 22:40:25 +0800

    新增leetcode [543. 二叉树的直径](https://leetcode.cn/problems/diameter-of-binary-tree/) 问题的求解

---
+++ src/main/java/basicalgorithm/tree/leetcode543/Solution.java
@@ -0,0 +1,38 @@
+package basicalgorithm.tree.leetcode543;
+
+import basicalgorithm.tree.base.TreeNode;
+import basicalgorithm.tree.base.TreeUtil;
+
+/**
+ * 543. 二叉树的直径
+ * https://leetcode.cn/problems/diameter-of-binary-tree/
+ */
+public class Solution {
+
+    int max = 0;
+
+    public static void main(String[] args) {
+        TreeNode root = TreeUtil.genTreeNode(new Integer[]{1, 2, 3, 4, 5});
+        Solution solution = new Solution();
+        int result = solution.diameterOfBinaryTree(root);
+        System.out.println(result);
+    }
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null) {
+            return max;
+        }
+        dfs(root);
+        return max;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDeep = dfs(root.left);
+        int rightDeep = dfs(root.right);
+        max = Math.max(max, leftDeep + rightDeep);
+        return 1 + Math.max(leftDeep, rightDeep);
+    }
+}
