6894c9e6f1fc24186069385f422eb9bf05734634
yun <yun160308@gmail.com>
2022-08-27 00:22:48 +0800

    新增leetcode [1026. 节点与其祖先之间的最大差值](https://leetcode.cn/problems/maximum-difference-between-node-and-ancestor/) 问题的求解

--- src/main/java/basicalgorithm/tree/leetcode1026/Solution.java
+++ src/main/java/basicalgorithm/tree/leetcode1026/Solution.java
@@ -5,6 +5,7 @@ import basicalgorithm.tree.base.TreeNode;
 /**
  * 1026. 节点与其祖先之间的最大差值
  * https://leetcode.cn/problems/maximum-difference-between-node-and-ancestor/
+ * todo review
  */
 public class Solution {
 
@@ -12,20 +13,31 @@ public class Solution {
         if (root == null) {
             return 0;
         }
+        // 默认情况下，已知的最小最大边界既是root.val
         return dfs(root, root.val, root.val);
     }
 
 
+    /**
+     * DFS
+     *
+     * @param root 树节点
+     * @param min  已知的最小边界值
+     * @param max  已知的最大边界值
+     * @return 最大差值
+     */
     private int dfs(TreeNode root, int min, int max) {
         if (root == null) {
+            // 直到root == null则说明到了最底层节点，此时该DFS路径的上下边界值已经是已知的最大范围，则计算差值
             return max - min;
         }
-
-        int leftMaxAncestorDiff = dfs(root.left, Math.min(min, root.val), Math.max(max, root.val));
-        int rightMaxAncestorDiff = dfs(root.right, Math.min(min, root.val), Math.max(max, root.val));
-        // todo 最终答案加不加这一行都是ok的，但是我觉得就得加这一行，为啥不加也行
-        int currentMaxAncestorDiff = max - min;
-        return Math.max(Math.max(leftMaxAncestorDiff, rightMaxAncestorDiff), currentMaxAncestorDiff);
+        // 每经过一个节点则根据当前节点值，重新计算最小边界和最大边界值（不断扩张已知的范围）
+        min = Math.min(min, root.val);
+        max = Math.max(max, root.val);
+        int leftMaxAncestorDiff = dfs(root.left, min, max);
+        int rightMaxAncestorDiff = dfs(root.right, min, max);
+        // 计算最大差值
+        return Math.max(leftMaxAncestorDiff, rightMaxAncestorDiff);
     }
 
 }
