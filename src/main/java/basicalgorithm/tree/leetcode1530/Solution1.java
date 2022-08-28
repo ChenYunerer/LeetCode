5c8fc0417bf30e970c4e8e6c6b400e2db4bf1801
yun <yun160308@gmail.com>
2022-08-28 23:37:30 +0800

    新增leetcode [1530. 好叶子节点对的数量](https://leetcode.cn/problems/number-of-good-leaf-nodes-pairs/) 问题的求解

---
+++ src/main/java/basicalgorithm/tree/leetcode1530/Solution1.java
@@ -0,0 +1,67 @@
+package basicalgorithm.tree.leetcode1530;
+
+import basicalgorithm.tree.base.TreeNode;
+
+/**
+ * 1530. 好叶子节点对的数量
+ * https://leetcode.cn/problems/number-of-good-leaf-nodes-pairs/
+ */
+public class Solution1 {
+
+    public int countPairs(TreeNode root, int distance) {
+        Pair pair = dfs(root, distance);
+        return pair.count;
+    }
+
+    // 对于 dfs(root,distance)，同时返回：
+    // 1）每个叶子节点与 root 之间的距离
+    // 2) 以 root 为根节点的子树中好叶子节点对的数量
+    // 由于本题中约束 distance≤10，因此对于每个非叶子节点 P，只需开辟长度为 distance+1 的数组，其中 depths[i] 表示与 P 之间的距离为 i 的叶子节点数目。
+    // 这个depths[i]用的太巧妙了，大大降低了复杂度 todo review
+    public Pair dfs(TreeNode root, int distance) {
+        int[] depths = new int[distance + 1];
+        boolean isLeaf = root.left == null && root.right == null;
+        if (isLeaf) {
+            depths[0] = 1;
+            return new Pair(depths, 0);
+        }
+
+        int[] leftDepths = new int[distance + 1];
+        int[] rightDepths = new int[distance + 1];
+        int leftCount = 0, rightCount = 0;
+        if (root.left != null) {
+            Pair leftPair = dfs(root.left, distance);
+            leftDepths = leftPair.depths;
+            leftCount = leftPair.count;
+        }
+        if (root.right != null) {
+            Pair rightPair = dfs(root.right, distance);
+            rightDepths = rightPair.depths;
+            rightCount = rightPair.count;
+        }
+
+        for (int i = 0; i < distance; i++) {
+            depths[i + 1] += leftDepths[i];
+            depths[i + 1] += rightDepths[i];
+        }
+
+        int cnt = 0;
+        for (int i = 0; i <= distance; i++) {
+            for (int j = 0; j + i + 2 <= distance; j++) {
+                cnt += leftDepths[i] * rightDepths[j];
+            }
+        }
+        return new Pair(depths, cnt + leftCount + rightCount);
+    }
+
+
+    class Pair {
+        int[] depths;
+        int count;
+
+        public Pair(int[] depths, int count) {
+            this.depths = depths;
+            this.count = count;
+        }
+    }
+}
