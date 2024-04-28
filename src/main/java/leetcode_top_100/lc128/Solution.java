a04304ed0e594bdcddc82a89e3b2febec8c826f7
yun <yun160308@gmail.com>
2024-04-28 17:11:41 +0800

    commit

--- src/main/java/leetcode_top_100/lc128/Solution.java
+++ src/main/java/leetcode_top_100/lc128/Solution.java
@@ -22,7 +22,27 @@ public class Solution {
     }
 
     // best
-    public int longestConsecutive(int[] nums) {
+    public int longestConsecutive0(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        int max = 0;
+        for (int num : nums) {
+            if (set.contains(num + 1)) {
+                continue;
+            }
+            int tempMax = 1;
+            while (set.contains(--num)) {
+                tempMax++;
+            }
+            max = Math.max(max, tempMax);
+        }
+        return max;
+    }
+
+    // best
+    public int longestConsecutive1(int[] nums) {
         Set<Integer> set = new HashSet<>();
         for (int num : nums) {
             set.add(num);
