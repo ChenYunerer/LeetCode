fcb50d1b40b6523f42c69b359fd24b38bb9731b7
yun <yun160308@gmail.com>
2024-04-29 16:54:56 +0800

    commit

---
+++ src/main/java/leetcode_top_100/double_point/lc42/Solution.java
@@ -0,0 +1,25 @@
+package leetcode_top_100.double_point.lc42;
+
+/**
+ * https://leetcode.cn/problems/trapping-rain-water/submissions/528138012/?envType=study-plan-v2&envId=top-100-liked
+ */
+public class Solution {
+
+    public int trap(int[] height) {
+        int ans = 0;
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        while (left < right) {
+            leftMax = Math.max(leftMax, height[left]);
+            rightMax = Math.max(rightMax, height[right]);
+            if (height[left] < height[right]) {
+                ans += leftMax - height[left];
+                ++left;
+            } else {
+                ans += rightMax - height[right];
+                --right;
+            }
+        }
+        return ans;
+    }
+}
