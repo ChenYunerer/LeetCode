6155817e2086a970bd04d7f6e27e8bbc405de7df
yun <yun160308@gmail.com>
2024-04-29 18:02:59 +0800

    commit

---
+++ src/main/java/leetcode_top_100/sliding_window/lc3/Solution.java
@@ -0,0 +1,25 @@
+package leetcode_top_100.sliding_window.lc3;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.cn/problems/longest-substring-without-repeating-characters/?envType=study-plan-v2&envId=top-100-liked
+ */
+public class Solution {
+
+    public int lengthOfLongestSubstring(String s) {
+        if (s.isEmpty()) return 0;
+        Map<Character, Integer> map = new HashMap<>();
+        int max = 0;
+        int left = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                left = Math.max(left, map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i), i);
+            max = Math.max(max, i - left + 1);
+        }
+        return max;
+    }
+}
