30bb696394a692048314ef0b823f983faddb3025
yun <yun160308@gmail.com>
2024-04-29 15:01:24 +0800

    commit

---
+++ src/main/java/leetcode_top_100/lc11/Solution.java
@@ -0,0 +1,21 @@
+package leetcode_top_100.lc11;
+
+/**
+ * https://leetcode.cn/problems/container-with-most-water/solutions/11491/container-with-most-water-shuang-zhi-zhen-fa-yi-do/?envType=study-plan-v2&envId=top-100-liked
+ */
+public class Solution {
+
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int max = 0;
+        while (left < right) {
+            max = Math.max(max, Math.min(height[left], height[right]) * (right - left));
+            if (height[left] < height[right]) {
+                left++;
+            }else {
+                right--;
+            }
+        }
+        return max;
+    }
+}
