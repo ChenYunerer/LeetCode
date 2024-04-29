fb9eda840ca3cece743ff90a5443391e1814ee67
yunyu.cy <yunyu.cy@alibaba-inc.com>
2024-04-29 22:07:22 +0800

    commit

--- src/main/java/leetcode_top_100/sliding_window/lc438/Solution.java
+++ src/main/java/leetcode_top_100/sliding_window/lc438/Solution.java
@@ -1,5 +1,7 @@
 package leetcode_top_100.sliding_window.lc438;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -8,6 +10,36 @@ import java.util.List;
 public class Solution {
 
     public List<Integer> findAnagrams(String s, String p) {
+        if (s == null || p == null || s.length() < p.length()) return new ArrayList<>();
+        int[] count1 = new int[26];
+        for (char c : p.toCharArray()) {
+            count1[c - 'a']++;
+        }
+        List<Integer> ans = new ArrayList<>();
+        int left = 0;
+        int right = 0;
+        int[] count2 = new int[26];
+        while (right <= s.length()) {
+            if (right < p.length()) {
+                count2[s.charAt(right) - 'a']++;
+                right++;
+            }
+            if (right - left == p.length()) {
+                if (Arrays.equals(count1, count2)) {
+                    ans.add(left);
+                }
+                count2[s.charAt(left) - 'a']--;
+                left++;
+                if (right < s.length()) {
+                    count2[s.charAt(right) - 'a']++;
+                }
+                right++;
+            }
+        }
+        return ans;
+    }
 
+    public static void main(String[] args) {
+        new Solution().findAnagrams("abab", "ab");
     }
 }
