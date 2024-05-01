d4c5a521e61327181d8efa27b8d4bca0c4a10f85
yun <yun160308@gmail.com>
2024-05-01 20:30:17 +0800

    commit

--- src/main/java/leetcode_top_100/sub_string/lc76/Solution.java
+++ src/main/java/leetcode_top_100/sub_string/lc76/Solution.java
@@ -4,35 +4,48 @@ import java.util.Arrays;
 
 public class Solution {
 
-    public String minWindow(String s, String t) {
-        if (s == null || t == null || s.isEmpty() || t.isEmpty()) return "";
-        int[] tCount = new int[26];
-        for (char c : t.toCharArray()) {
-            tCount[c - 'a']++;
-        }
-        int[] sCount = new int[26];
-        for (int i = 0; i < t.length(); i++) {
-            if (tCount[s.charAt(i) - 'a'] > 0) {
-                sCount[s.charAt(i) - 'a']++;
-            }
-        }
-        if (Arrays.equals(tCount, sCount)) {
-            return s.substring(0, t.length());
-        }
-        String subStr = s;
-        int i = 0;
-        int j = t.length();
-        for (; j < s.length(); j++) {
-            if (tCount[s.charAt(j) - 'a'] > 0) {
-                sCount[s.charAt(j) - 'a']++;
-            }
-            if (Arrays.equals(tCount, sCount)) {
-                if (j - i < subStr.length()) {
-                    subStr = s.substring(i, j + 1);
-                }
-            }
+    Map<Character, Integer> ori = new HashMap<Character, Integer>();
+    Map<Character, Integer> cnt = new HashMap<Character, Integer>();
 
+    public String minWindow(String s, String t) {
+        int tLen = t.length();
+        for (int i = 0; i < tLen; i++) {
+            char c = t.charAt(i);
+            ori.put(c, ori.getOrDefault(c, 0) + 1);
         }
-        return subStr;
+        int l = 0, r = -1;
+        int len = Integer.MAX_VALUE, ansL = -1, ansR = -1;
+        int sLen = s.length();
+        while (r < sLen) {
+            ++r;
+            if (r < sLen && ori.containsKey(s.charAt(r))) {
+                cnt.put(s.charAt(r), cnt.getOrDefault(s.charAt(r), 0) + 1);
+            }
+            while (check() && l <= r) {
+                if (r - l + 1 < len) {
+                    len = r - l + 1;
+                    ansL = l;
+                    ansR = l + len;
+                }
+                if (ori.containsKey(s.charAt(l))) {
+                    cnt.put(s.charAt(l), cnt.getOrDefault(s.charAt(l), 0) - 1);
+                }
+                ++l;
+            }
+        }
+        return ansL == -1 ? "" : s.substring(ansL, ansR);
+    }
+
+    public boolean check() {
+        Iterator iter = ori.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry entry = (Map.Entry) iter.next();
+            Character key = (Character) entry.getKey();
+            Integer val = (Integer) entry.getValue();
+            if (cnt.getOrDefault(key, 0) < val) {
+                return false;
+            }
+        }
+        return true;
     }
 }
