84246c2a8c11f793d50a98c7e6301def0b0f80db
yun <yun160308@gmail.com>
2024-05-03 16:46:52 +0800

    commit

--- src/main/java/leetcode_top_100/matrix/lc73/Solution.java
+++ src/main/java/leetcode_top_100/matrix/lc73/Solution.java
@@ -9,6 +9,10 @@ import java.util.Set;
  */
 public class Solution {
 
+    public static void main(String[] args) {
+        new Solution().setZeroes1(new int[][]{{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}});
+    }
+
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
@@ -27,6 +31,44 @@ public class Solution {
                 if (cols.contains(i) || rows.contains(j)) {
                     matrix[i][j] = 0;
                 }
+            }
+        }
+    }
+
+    public void setZeroes1(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean firstCol = false;
+        boolean firstRow = false;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    if (i == 0) {
+                        firstCol = true;
+                    }
+                    if (j == 0) {
+                        firstRow = true;
+                    }
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (firstRow) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        if (firstCol) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
             }
         }
     }
