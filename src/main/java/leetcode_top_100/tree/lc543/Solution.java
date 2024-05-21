c3894daabb224a4e453f7cf73f9cd845096b07ac
yun <yun160308@gmail.com>
2024-05-22 00:14:34 +0800

    commit

---
+++ src/main/java/leetcode_top_100/tree/lc543/Solution.java
@@ -0,0 +1,14 @@
+package leetcode_top_100.tree.lc543;
+
+import basicalgorithm.tree.base.TreeNode;
+
+/**
+ * 543. 二叉树的直径
+ * https://leetcode.cn/problems/diameter-of-binary-tree/description/?envType=study-plan-v2&envId=top-100-liked
+ */
+public class Solution {
+
+    public int diameterOfBinaryTree(TreeNode root) {
+
+    }
+}
