e863b9542c2401a4ab19944d7c109470c59dbc73
yun <yun160308@gmail.com>
2024-05-22 00:05:29 +0800

    commit

--- src/main/java/leetcode_top_100/tree/lc101/Solution.java
+++ src/main/java/leetcode_top_100/tree/lc101/Solution.java
@@ -5,6 +5,16 @@ import basicalgorithm.tree.base.TreeNode;
 public class Solution {
 
     public boolean isSymmetric(TreeNode root) {
+        return isSymmetric(root.left, root.right);
+    }
 
+    public boolean isSymmetric(TreeNode a, TreeNode b) {
+        if ((a == null && b != null) || (a != null && b == null)) {
+            return false;
+        } else if (a == null && b == null) {
+            return true;
+        } else {
+            return a.val == b.val && isSymmetric(a.left, b.right) && isSymmetric(a.right, b.left);
+        }
     }
 }
