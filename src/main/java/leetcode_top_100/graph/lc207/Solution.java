3e347d1453c8cb1abe8cf1b82482af6b824ff4e1
yun <yun160308@gmail.com>
2024-05-30 03:23:29 +0800

    commit

---
+++ src/main/java/leetcode_top_100/graph/lc207/Solution.java
@@ -0,0 +1,12 @@
+package leetcode_top_100.graph.lc207;
+
+/**
+ * 207. 课程表
+ * https://leetcode.cn/problems/course-schedule/description/?envType=study-plan-v2&envId=top-100-liked
+ */
+public class Solution {
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+    }
+}
