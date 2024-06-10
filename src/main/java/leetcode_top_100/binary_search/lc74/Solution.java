abf62d357903d8b0a9a4f0aa1ae4cf2a34ac5a25
yun <yun160308@gmail.com>
2024-06-10 12:40:39 +0800

    commit

--- src/main/java/leetcode_top_100/binary_search/lc74/Solution.java
+++ src/main/java/leetcode_top_100/binary_search/lc74/Solution.java
@@ -7,7 +7,25 @@ package leetcode_top_100.binary_search.lc74;
 public class Solution {
 
     public boolean searchMatrix(int[][] matrix, int target) {
-
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        int allCount = rows * columns;
+        int left = 0;
+        int right = allCount - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int row = mid / columns;
+            int column = mid % columns;
+            if (matrix[row][column] == target) {
+                return true;
+            }
+            if (matrix[row][column] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return false;
     }
 
 }
