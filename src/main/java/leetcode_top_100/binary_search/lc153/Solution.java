91df387f2b0fb289e6862a11df8f49fd3b05bd33
yun <yun160308@gmail.com>
2024-06-13 01:26:54 +0800

    commit

--- src/main/java/leetcode_top_100/binary_search/lc153/Solution.java
+++ src/main/java/leetcode_top_100/binary_search/lc153/Solution.java
@@ -6,7 +6,25 @@ package leetcode_top_100.binary_search.lc153;
  */
 public class Solution {
 
-    public int findMin(int[] nums) {
+    public static void main(String[] args) {
+        int ans = new Solution().findMin(new int[]{3, 1, 2});
+        System.out.println(ans);
+    }
 
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int min = nums[0];
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[left] <= nums[mid]) {
+                min = Math.min(nums[left], min);
+                left = mid + 1;
+            } else {
+                min = Math.min(nums[mid], min);
+                right = mid - 1;
+            }
+        }
+        return min;
     }
 }
