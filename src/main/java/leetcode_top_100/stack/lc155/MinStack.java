cd9bddb8eb210a04ed13bb68d92a0d79eace23e3
yun <yun160308@gmail.com>
2024-06-20 19:29:04 +0800

    commit

---
+++ src/main/java/leetcode_top_100/stack/lc155/MinStack.java
@@ -0,0 +1,28 @@
+package leetcode_top_100.stack.lc155;
+
+/**
+ * 155. 最小栈
+ * https://leetcode.cn/problems/min-stack/description/?envType=study-plan-v2&envId=top-100-liked
+ */
+public class MinStack {
+
+    public MinStack() {
+
+    }
+
+    public void push(int val) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+
+    }
+
+    public int getMin() {
+
+    }
+}
---
+++ src/main/java/other/Test3.java
@@ -0,0 +1,20 @@
+package other;
+
+
+/**
+ * Question description
+ * p = [['a','b'], ['b','c'],['d','e']...]
+ * v = [2,3,4...]
+ * q = [['a','c'], ['a','d'],...]
+ * <p>
+ * 'a' / 'b' = 2
+ * 'b' / 'c' = 3
+ * 'd' / 'e' = 4
+ * <p>
+ * 求 a/c=? a/d=?
+ * 输出：[6, -1]
+ */
+public class Test3 {
+
+
+}
