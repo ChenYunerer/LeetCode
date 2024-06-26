48c47184e37f3611c10246bcb26fc5cecf13fc75
yun <yun160308@gmail.com>
2024-06-27 00:55:19 +0800

    commit

--- src/main/java/leetcode_top_100/stack/lc739/Solution.java
+++ src/main/java/leetcode_top_100/stack/lc739/Solution.java
@@ -1,5 +1,8 @@
 package leetcode_top_100.stack.lc739;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * 739. 每日温度
  * https://leetcode.cn/problems/daily-temperatures/description/?envType=study-plan-v2&envId=top-100-liked
@@ -7,6 +10,15 @@ package leetcode_top_100.stack.lc739;
 public class Solution {
 
     public int[] dailyTemperatures(int[] temperatures) {
-
+        Deque<Integer> stack = new LinkedList<>();
+        int[] ans = new int[temperatures.length];
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                Integer index = stack.pop();
+                ans[index] = i - index;
+            }
+            stack.push(i);
+        }
+        return ans;
     }
 }
