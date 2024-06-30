cf7a1460797c3c651716485b3dacd0cd789c198b
yun <yun160308@gmail.com>
2024-07-01 00:13:36 +0800

    commit

---
+++ src/main/java/leetcode_top_150/matrix/lc36/Solution.java
@@ -0,0 +1,12 @@
+package leetcode_top_150.matrix.lc36;
+
+/**
+ * 36. 有效的数独
+ * https://leetcode.cn/problems/valid-sudoku/description/?envType=study-plan-v2&envId=top-interview-150
+ */
+public class Solution {
+
+    public boolean isValidSudoku(char[][] board) {
+
+    }
+}
