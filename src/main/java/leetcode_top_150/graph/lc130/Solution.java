515feff415b58b64a00dc49847ff63be6714a1ef
yun <yun160308@gmail.com>
2024-07-01 02:40:03 +0800

    commit

---
+++ src/main/java/leetcode_top_150/graph/lc130/Solution.java
@@ -0,0 +1,8 @@
+package leetcode_top_150.graph.lc130;
+
+/**
+ * 130. 被围绕的区域
+ * https://leetcode.cn/problems/surrounded-regions/description/?envType=study-plan-v2&envId=top-interview-150
+ */
+public class Solution {
+}
