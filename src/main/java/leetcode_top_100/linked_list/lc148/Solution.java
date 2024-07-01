a16522b613a7897ed63d0d583c361457042ccf42
yun <yun160308@gmail.com>
2024-07-02 02:10:00 +0800

    commit

---
+++ src/main/java/leetcode_top_100/linked_list/lc148/Solution.java
@@ -0,0 +1,16 @@
+package leetcode_top_100.linked_list.lc148;
+
+import categorized.linkedList.base.ListNode;
+
+/**
+ * 148. 排序链表
+ * https://leetcode.cn/problems/sort-list/description/?envType=study-plan-v2&envId=top-100-liked
+ */
+public class Solution {
+
+    public ListNode sortList(ListNode head) {
+        return sortList(head, null);
+    }
+
+
+}
