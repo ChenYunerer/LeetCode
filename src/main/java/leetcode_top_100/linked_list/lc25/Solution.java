7bc24906c1cbe792a2b2a7c6ccf269978a6256f4
yun <yun160308@gmail.com>
2024-07-01 23:25:44 +0800

    commit

--- src/main/java/leetcode_top_100/linked_list/lc25/Solution.java
+++ src/main/java/leetcode_top_100/linked_list/lc25/Solution.java
@@ -1,6 +1,7 @@
 package leetcode_top_100.linked_list.lc25;
 
 import categorized.linkedList.base.ListNode;
+import categorized.linkedList.base.ListNodeUtil;
 
 /**
  * 25. K 个一组翻转链表
@@ -8,43 +9,49 @@ import categorized.linkedList.base.ListNode;
  */
 public class Solution {
 
-    public ListNode reverseKGroup(ListNode head, int k) {
-        ListNode hair = new ListNode(0);
-        hair.next = head;
-        ListNode pre = hair;
-
-        while (head != null) {
-            ListNode tail = pre;
-            // 查看剩余部分长度是否大于等于 k
-            for (int i = 0; i < k; i++) {
-                tail = tail.next;
-                if (tail == null) {
-                    return hair.next;
-                }
-            }
-            ListNode nex = tail.next;
-            ListNode[] reverse = myReverse(head, tail);
-            head = reverse[0];
-            tail = reverse[1];
-            // 把子链表重新接回原链表
-            pre.next = head;
-            tail.next = nex;
-            pre = tail;
-            head = tail.next;
-        }
-
-        return hair.next;
+    public static void main(String[] args) {
+        ListNode head = ListNodeUtil.genListNode(new int[]{1, 2, 3, 4, 5});
+        new Solution().reverseKGroup(head, 3);
     }
 
-    public ListNode[] myReverse(ListNode head, ListNode tail) {
-        ListNode prev = tail.next;
-        ListNode p = head;
-        while (prev != tail) {
-            ListNode nex = p.next;
-            p.next = prev;
-            prev = p;
-            p = nex;
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode cur = head;
+        A:
+        while (cur != null) {
+            ListNode tail = cur;
+            for (int i = 0; i < k - 1; i++) {
+                if (tail.next != null) {
+                    tail = tail.next;
+                } else {
+                    break A;
+                }
+            }
+            ListNode tailNext = tail.next;
+            ListNode[] ans = reverseListNode(cur, tail);
+            ListNode newHead = ans[0];
+            ListNode newTail = ans[1];
+            pre.next = newHead;
+            newTail.next = tailNext;
+
+            pre = newTail;
+            cur = tailNext;
+        }
+        return dummy.next;
+    }
+
+    private ListNode[] reverseListNode(ListNode head, ListNode tail) {
+        ListNode pre = head;
+        ListNode cur = head.next;
+        while (pre != tail) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
         }
         return new ListNode[]{tail, head};
     }
+
 }
