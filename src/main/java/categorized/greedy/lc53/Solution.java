17077b7c8ecdd7c0946d6afb79e1476bf4cb495a
yun <yun160308@gmail.com>
2024-07-04 00:52:59 +0800

    commit

---
+++ src/main/java/categorized/greedy/lc53/Solution.java
@@ -0,0 +1,23 @@
+package categorized.greedy.lc53;
+
+/**
+ * 53. 最大子数组和
+ * https://leetcode.cn/problems/maximum-subarray/description/
+ */
+public class Solution {
+
+    public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int preSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (preSum >= 0) {
+                preSum += nums[i];
+            } else {
+                preSum = nums[i];
+            }
+            maxSum = Math.max(maxSum, preSum);
+        }
+        return maxSum;
+    }
+
+}
