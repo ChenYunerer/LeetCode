b9780abdde4a128abe8dcfa4099fd538fe464c72
yun <yun160308@gmail.com>
2024-11-02 18:53:12 +0800

    commit

--- src/main/java/leetcode_top_100/sub_string/lc239/Solution.java
+++ src/main/java/leetcode_top_100/sub_string/lc239/Solution.java
@@ -1,15 +1,36 @@
 package leetcode_top_100.sub_string.lc239;
 
+
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 
 /**
+ * 239. 滑动窗口最大值
  * https://leetcode.cn/problems/sliding-window-maximum/?envType=study-plan-v2&envId=top-100-liked
  */
 public class Solution {
 
-    // 单调栈
+    // 优先级队列
     public int[] maxSlidingWindow(int[] nums, int k) {
+        PriorityQueue<int[]> priorityQueue = new PriorityQueue<>((o1, o2) -> o2[0] - o1[0]);
+        for (int i = 0; i < k; i++) {
+            priorityQueue.offer(new int[]{nums[i], i});
+        }
+        int[] ans = new int[nums.length - k + 1];
+        ans[0] = priorityQueue.peek()[0];
+        for (int i = k; i < nums.length; i++) {
+            priorityQueue.offer(new int[]{nums[i], i});
+            while (priorityQueue.peek()[1] <= i - k) {
+                priorityQueue.poll();
+            }
+            ans[i - k + 1] = priorityQueue.peek()[0];
+        }
+        return ans;
+    }
+
+    // 单调队列
+    public int[] maxSlidingWindow1(int[] nums, int k) {
         Deque<Integer> deque = new LinkedList<>();
         for (int i = 0; i < k; i++) {
             while (!deque.isEmpty() && nums[i] > nums[deque.peekLast()]) {
