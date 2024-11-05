f8aba9218895453ad1d976c7849240fb37c4aad8
yun <yun160308@gmail.com>
2024-11-06 00:36:47 +0800

    commit

--- src/main/java/leetcode_top_100/normal_array/lc53/Solution.java
+++ src/main/java/leetcode_top_100/normal_array/lc53/Solution.java
@@ -6,31 +6,29 @@ package leetcode_top_100.normal_array.lc53;
  */
 public class Solution {
 
-    // 贪心
+    // 前缀和
     public int maxSubArray(int[] nums) {
-        int max = nums[0];
-        int mMax = 0;
-        for (int start = 0; start < nums.length; start++) {
-            mMax = mMax + nums[start];
-            if (mMax > max) {
-                max = mMax;
-            }
-            if (mMax < 0) {
-                mMax = 0;
-            }
+        int ans = Integer.MIN_VALUE;
+        int preSum = 0;
+        int minPreSum = preSum;
+        for (int x : nums) {
+            preSum = preSum + x;
+            ans = Math.max(ans, preSum - minPreSum);
+            minPreSum = Math.min(minPreSum, preSum);
         }
-        return max;
+        return ans;
     }
 
-    // 前缀和
+    // 贪心
     public int maxSubArray1(int[] nums) {
-        int ans = Integer.MIN_VALUE;
-        int minPreSum = 0;
-        int preSum = 0;
+        int ans = nums[0];
+        int subArraySum = 0;
         for (int x : nums) {
-            preSum += x; // 当前的前缀和
-            ans = Math.max(ans, preSum - minPreSum); // 减去前缀和的最小值
-            minPreSum = Math.min(minPreSum, preSum); // 维护前缀和的最小值
+            subArraySum = subArraySum + x;
+            ans = Math.max(ans, subArraySum);
+            if (subArraySum < 0) {
+                subArraySum = 0;
+            }
         }
         return ans;
     }
