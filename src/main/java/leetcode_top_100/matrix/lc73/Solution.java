dedd18adf31940e6ec0ffca2c21f7918ad2c1b5d
yun <yun160308@gmail.com>
2024-11-07 00:50:19 +0800

    commit

--- src/main/java/leetcode_top_100/matrix/lc73/Solution.java
+++ src/main/java/leetcode_top_100/matrix/lc73/Solution.java
@@ -1,39 +1,10 @@
 package leetcode_top_100.matrix.lc73;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * 73. 矩阵置零
  * https://leetcode.cn/problems/set-matrix-zeroes/description/?envType=study-plan-v2&envId=top-100-liked
  */
 public class Solution {
-
-    public static void main(String[] args) {
-        new Solution().setZeroes1(new int[][]{{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}});
-    }
-
-    public void setZeroes(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        Set<Integer> cols = new HashSet<>();
-        Set<Integer> rows = new HashSet<>();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    cols.add(i);
-                    rows.add(j);
-                }
-            }
-        }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (cols.contains(i) || rows.contains(j)) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-    }
 
     public void setZeroes1(int[][] matrix) {
         int rows = matrix.length;
