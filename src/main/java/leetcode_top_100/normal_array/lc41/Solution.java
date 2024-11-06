f65c1a3b5faef6d0554276a06da29abb40a726ad
yun <yun160308@gmail.com>
2024-11-07 00:12:19 +0800

    commit

--- src/main/java/leetcode_top_100/normal_array/lc41/Solution.java
+++ src/main/java/leetcode_top_100/normal_array/lc41/Solution.java
@@ -6,21 +6,50 @@ package leetcode_top_100.normal_array.lc41;
  */
 public class Solution {
 
+    public static void main(String[] args) {
+        int ans = new Solution().firstMissingPositive(new int[]{3, 4, -1, 1});
+        System.out.println(ans);
+    }
+
+    /**
+     * 置换法
+     */
     public int firstMissingPositive(int[] nums) {
-        if (nums == null || nums.length == 0) return 0;
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] - 1 >= 0 && nums[i] - 1 < nums.length && nums[nums[i] - 1] != nums[i]) {
                 int temp = nums[nums[i] - 1];
                 nums[nums[i] - 1] = nums[i];
                 nums[i] = temp;
             }
         }
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < nums.length; i++) {
             if (nums[i] != i + 1) {
                 return i + 1;
             }
         }
-        return n + 1;
+        return nums.length + 1;
+    }
+
+    /**
+     * 标记法
+     */
+    public int firstMissingPositive0(int[] nums) {
+        int length = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= 0) {
+                nums[i] = length + 1;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (Math.abs(nums[i]) - 1 < length && nums[Math.abs(nums[i]) - 1] > 0) {
+                nums[Math.abs(nums[i]) - 1] = -nums[Math.abs(nums[i]) - 1];
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= 0) {
+                return i + 1;
+            }
+        }
+        return length + 1;
     }
 }
