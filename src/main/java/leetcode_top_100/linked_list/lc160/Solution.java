180e1223731aeef8f2b8b09c8ad6306a85c2976b
yun <yun160308@gmail.com>
2024-11-07 23:29:57 +0800

    commit

--- src/main/java/leetcode_top_100/linked_list/lc160/Solution.java
+++ src/main/java/leetcode_top_100/linked_list/lc160/Solution.java
@@ -27,4 +27,17 @@ public class Solution {
         }
         return null;
     }
+
+    public ListNode getIntersectionNode1(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+
 }
